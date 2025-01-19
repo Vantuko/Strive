d14872be4a803c408940b0af2c1c86d49996d845
Vantuko <vantukopro@gmail.com>
2025-01-19 18:23:48 -0300

    Bug levitacion purga arreglado

--- src/main/java/com/badstudio/purga/listeners/Events.java
+++ src/main/java/com/badstudio/purga/listeners/Events.java
@@ -2,15 +2,21 @@ package com.badstudio.purga.listeners;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityPotionEffectEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.ChatColor;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Events implements Listener{
     @EventHandler
     public void PlayerEnter(PlayerJoinEvent e){
         Player player = e.getPlayer();
 
+        if(player.hasPotionEffect(PotionEffectType.LEVITATION)){
+            player.removePotionEffect(PotionEffectType.LEVITATION);
+        }
 
         e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', "&7[&a+&7] &a"+player.getName()));
     }
@@ -22,4 +28,6 @@ public class Events implements Listener{
         e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', "&7[&c-&7] &c"+player.getName()));
 
     }
+
+
 }

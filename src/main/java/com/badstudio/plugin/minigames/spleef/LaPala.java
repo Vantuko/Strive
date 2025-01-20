d62815581769ca13777a2e7f7c3692fd5a9bbe87
SMyBad <smybadd@gmail.com>
2025-01-20 19:47:44 -0300

    Agregado un nuevo evento donde al tirar una snowball te rompe un bloque de nieve

--- src/main/java/com/badstudio/plugin/minigames/spleef/LaPala.java
+++ src/main/java/com/badstudio/plugin/minigames/spleef/LaPala.java
@@ -3,10 +3,14 @@ package com.badstudio.plugin.minigames.spleef;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
@@ -19,7 +23,7 @@ public class LaPala implements Listener {
     private static final int finalBlock = 20;
 
     @EventHandler
-    public void OnBreakSnow(BlockBreakEvent e) {
+    public void onBreakSnow(BlockBreakEvent e) {
         World mundo = Bukkit.getWorld("Spleef");
 
         if (mundo != null && e.getBlock().getWorld().equals(mundo)) {
@@ -29,7 +33,7 @@ public class LaPala implements Listener {
                 Player jugador = e.getPlayer();
                 UUID jugadorID = jugador.getUniqueId();
 
-                bloquesDestruidos.put(jugadorID, bloquesDestruidos.getOrDefault(jugadorID, 0)+ 1);
+                bloquesDestruidos.put(jugadorID, bloquesDestruidos.getOrDefault(jugadorID, 0) + 1);
 
                 if (bloquesDestruidos.get(jugadorID) >= finalBlock) {
                     jugador.getInventory().addItem(new ItemStack(Material.SNOWBALL, 1));
@@ -42,5 +46,22 @@ public class LaPala implements Listener {
             Bukkit.getLogger().warning("El mundo Spleef no existe!");
         }
     }
-}
 
+    @EventHandler
+    public void onSnowballThrow(ProjectileHitEvent e) {
+        Projectile snowBall = e.getEntity();
+        if (snowBall instanceof Snowball) {
+            World mundo = snowBall.getWorld();
+
+            if (mundo.getName().equalsIgnoreCase("Spleef")) {
+                Block blockBreak = e.getHitBlock();
+
+                if (blockBreak != null && blockBreak.getType() == Material.SNOW_BLOCK) {
+                    blockBreak.setType(Material.AIR);
+                }
+            } else {
+                Bukkit.getLogger().warning("No estas en el mundo de Spleef!");
+            }
+        }
+    }
+}

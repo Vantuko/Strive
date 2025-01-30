412fc5237a4d1d957d46adfd9468534f834b6b14
Vantuko <vantukopro@gmail.com>
2025-01-30 14:27:34 -0300

    comando spleefstop

---
+++ src/main/java/com/badstudio/plugin/minigames/spleef/SpleefStop.java
@@ -0,0 +1,24 @@
+package com.badstudio.plugin.minigames.spleef;
+
+import com.badstudio.plugin.minigames.spleef.utils.Bossbar;
+import com.badstudio.plugin.utils.GuardarMapa;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class SpleefStop implements CommandExecutor {
+    private final GuardarMapa guardarMapa = new GuardarMapa();
+    private Bossbar bossbar;
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        guardarMapa.restaurarMapa(Bukkit.getWorld("Spleef"));
+        guardarMapa.limpiarDatos();
+        bossbar.Finalizacion();
+        Spleef.setJuegoActivo(false);
+        return false;
+    }
+}
